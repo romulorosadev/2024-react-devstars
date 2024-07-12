4e27ee0060b634f5a00ddcfeff4caadb6a7bafc8
Romulo Guilherme Rosa | @romulorosadev <romulorosadev@gmail.com>
2024-07-11 21:29:10 -0300

    css variables

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css"
 import Footer from "./components/Footer/Footer"
 import Header from "./components/Header/Header"
 import Main from "./components/Main/Main"
+import "./variables.css"
 
 export default function App() {
   return (
